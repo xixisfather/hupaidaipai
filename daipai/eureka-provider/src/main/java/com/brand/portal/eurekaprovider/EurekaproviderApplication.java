c9bef543527dd0776d16f6adbdbe7c30985c700c
xixisfather <chenchunrongchun@163.com>
2020-04-03 14:56:09 +0800

    no message

---
+++ daipai/eureka-provider/src/main/java/com/brand/portal/eurekaprovider/EurekaproviderApplication.java
@@ -0,0 +1,13 @@
+package com.brand.portal.eurekaprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaproviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaproviderApplication.class, args);
+    }
+
+}
---
+++ daipai/eureka-server/src/test/java/com/brand/protal/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brand.protal.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
